4ed03e02fa9ec1aaeef2a24ad6f8c527f70b4e78
funkysys <aadelbos@gmail.com>
2023-03-04 18:20:18 +0100

    upgrade login and registration form styles

--- migrations/Version20230303220030.php
+++ migrations/Version20230303220030.php
@@ -22,6 +22,26 @@ final class Version20230303220030 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $allergns = array('Gluten', 'Arachide', 'Lait', 'Oeufs', 'Fruits à coque', 'Mollusques', 'Fruits de mer', 'Moutarde', 'Poisson', 'Céleri', 'Soja', 'Sulfites', 'Sésame', 'Lupin');
+        foreach ($allergns as $allergn) {
+            $this->addSql('INSERT INTO allergy (name) VALUES (?)', array($allergn));
+        }
+
+        $days = array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+        foreach ($days as $day) {
+            $this->addSql('INSERT INTO days (day) VALUES (?)', array($day));
+        }
+
+        $hours = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
+        foreach ($hours as $hour) {
+            $this->addSql('INSERT INTO hours (hour) VALUES (?)', array($hour));
+        }
+
+        $minutes = array(0, 15, 30, 45);
+
+        foreach ($minutes as $minute) {
+            $this->addSql('INSERT INTO minutes (minutes) VALUES (?)', array($minute));
+        }
     }
 
     public function down(Schema $schema): void
