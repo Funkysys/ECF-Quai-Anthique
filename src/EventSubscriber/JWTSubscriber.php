9093f4066a1ff24e69b9402c0131a8dd188ff8b2
funkysys <aadelbos@gmail.com>
2023-03-08 21:37:57 +0100

    add resolution conflict for JWTSubscriber

---
+++ src/EventSubscriber/JWTSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class JWTSubscriber implements EventSubscriberInterface
+{
+    public function onLexikJwtAuthenticationOnJwtCreated(JWTCreatedEvent $event): void
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['id'] = $user->getId();
+        $data['name'] = $user->getName();
+        
+        $event->setData($data);
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'lexik_jwt_authentication.on_jwt_created' => 'onLexikJwtAuthenticationOnJwtCreated',
+        ];
+    }
+}
